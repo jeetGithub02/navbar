dbcf61ae89d417ea5e1a39e34efb82af418e8b13
Jitendra <jeettech02@gmail.com>
2024-06-19 00:05:34 +0530

    update navigation machanism

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react'
 import { RiMenu3Line } from "react-icons/ri";
 import { FaTimes } from "react-icons/fa";
 const Navbar2 = () => {
-    const[top,setTop]=useState('-100vh')
+    // const[top,setTop]=useState('-200vh')
+    const[active,setActive]=useState(false)
 
     function handleMenuOpen(){
-          setTop("0px")     
+        setActive(true)   
     }
     function handleMenuClose(){
-        setTop("-100vh")     
+        setActive(false)     
   }
   return (
     <header className='flex relative items-center justify-between h-[70px] bg-[#10154c] px-[40px] text-white'>
         <div className='text-[25px] font-medium'>ROOMIES</div>
-        <nav className={`top-[${top}] duration-200 flex gap-5  md:flex-row flex-col bg-[#10154c]  absolute top-0 left-0 w-[100%] items-center md:static h-[60vh] justify-center md:justify-normal md:w-auto md:h-auto`}>
+        <nav className={`${active?"top-0":"top-[-100vh]"} duration-200 flex gap-5  md:flex-row flex-col bg-[#10154c]  absolute top-0 left-0 w-[100%] items-center md:static h-[100vh] justify-center md:justify-normal md:w-auto md:h-auto`}>
             <div className='block md:hidden absolute top-[25px] right-[40px]'  onClick={handleMenuClose}><FaTimes /></div>
             <a href="">HOSTEL</a>
             <a href="">ROOM</a>
