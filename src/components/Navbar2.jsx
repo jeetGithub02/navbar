61404aecc97b6d89254f2258362212751c844397
Jitendra <jeettech02@gmail.com>
2024-06-19 00:09:17 +0530

    update menu Icon size

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -15,13 +15,13 @@ const Navbar2 = () => {
     <header className='flex relative items-center justify-between h-[70px] bg-[#10154c] px-[40px] text-white'>
         <div className='text-[25px] font-medium'>ROOMIES</div>
         <nav className={`${active?"top-0":"top-[-100vh]"} duration-200 flex gap-5  md:flex-row flex-col bg-[#10154c]  absolute top-0 left-0 w-[100%] items-center md:static h-[100vh] justify-center md:justify-normal md:w-auto md:h-auto`}>
-            <div className='block md:hidden absolute top-[25px] right-[40px]'  onClick={handleMenuClose}><FaTimes /></div>
+            <div className='block md:hidden absolute top-[25px] right-[40px]'  onClick={handleMenuClose}><FaTimes className='text-xl' /></div>
             <a href="">HOSTEL</a>
             <a href="">ROOM</a>
             <a href="">SEARCH</a>
             <a href="">MY BOOKINGS</a>
         </nav>
-        <div className='block md:hidden' onClick={handleMenuOpen}><RiMenu3Line /></div>
+        <div className='block md:hidden' onClick={handleMenuOpen}><RiMenu3Line className="text-xl" /></div>
     </header>
   )
 }
